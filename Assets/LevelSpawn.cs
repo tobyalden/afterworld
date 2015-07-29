2140f9b134cfe75528a757fb770d3e140c3979d0
Alec Arme <Incubusalec@gmail.com>
2015-07-29 16:47:04 -0700

    Add light crossfade on level spawn

--- Assets/LevelSpawn.cs
+++ Assets/LevelSpawn.cs
@@ -6,8 +6,12 @@ public class LevelSpawn : MonoBehaviour {
 	public int spawnTime;
 	float timer;
 	bool spawned;
+	bool fadingOut;
+	bool fadingIn;
 	GameObject oldRayCaster;
 	GameObject oldLight;
+	public Light oldDirLight;
+	public Light newDirLight;
 	// Use this for initialization
 	void Start () {
 		timer = 0;
@@ -22,12 +26,42 @@ public class LevelSpawn : MonoBehaviour {
 
 		if (timer >= spawnTime && !spawned)
 		{
-			spawned = true;
-			Destroy(oldRayCaster);
-			Destroy(oldLight);
-			Instantiate(levelGroup);
+			fadingOut = true;
+
+
+//			Instantiate(levelGroup);
 
 		}
+
+		if (fadingOut && oldDirLight.intensity > .1)
+		{
+			Debug.Log("Light Intensity: " + oldDirLight.intensity);
+			oldDirLight.intensity = Mathf.Lerp(oldDirLight.intensity, 0f, 1f * Time.deltaTime);
+
+
+			if (oldDirLight.intensity < .1)
+			{
+				oldDirLight.intensity = 0;
+				fadingOut = false;
+				Destroy(oldRayCaster);
+				Instantiate(levelGroup);
+				spawned = true;
+				fadingIn = true;
+			}
+		}
+
+		if (fadingIn) 
+		{
+			newDirLight.intensity = Mathf.Lerp(newDirLight.intensity, 8f, .1f * Time.deltaTime);
+			Debug.Log ("New light intensity: " + newDirLight.intensity);
+			if (newDirLight.intensity > 7.9)
+			{
+				newDirLight.intensity = 8;
+				fadingIn = false;
+			}
+
+		}
+
 
 	}
 }
